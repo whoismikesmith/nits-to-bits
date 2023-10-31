e2bbd972a9e02fb90fb9e8d9d7442a557c7fcd62
Mike Smith <whereismikesmith@gmail.com>
2023-10-30 21:58:07 -0700

    add gerbers and kicad project

---
+++ code/nits_to_bits/nits_to_bits.ino
@@ -0,0 +1,77 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+// The pins for I2C are defined by the Wire-library. 
+// On an arduino UNO:       A4(SDA), A5(SCL)
+// On an arduino MEGA 2560: 20(SDA), 21(SCL)
+// On an arduino LEONARDO:   2(SDA),  3(SCL), ...
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+unsigned long ontime;                    
+unsigned long offtime;
+unsigned long dutycycle;
+unsigned long period;
+float frequency;
+float capacitance;
+
+void setup()
+{
+    pinMode(8,INPUT);            //pin 8 as signal input
+    Serial.begin(9600);
+
+     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  display.setRotation(2);
+    // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+
+  // Draw a single pixel in white
+  display.drawPixel(10, 10, SSD1306_WHITE);
+
+  // Show the display buffer on the screen. You MUST call display() after
+  // drawing commands to make them visible on screen!
+  display.display();
+  delay(2000);
+}
+
+void loop()
+{
+    //noInterrupts();
+    ontime = pulseIn(8,HIGH);
+    offtime = pulseIn(8,LOW);
+    //interrupts();
+
+    period = ontime+offtime;
+    dutycycle = 100-(ontime/offtime);
+    frequency = 1000000.0/period;
+    Serial.print(frequency);
+    Serial.println("hz");
+    Serial.print(dutycycle);
+    Serial.println("%");
+    Serial.flush();
+
+    display.clearDisplay();
+    display.setTextSize(2);      // Normal 1:1 pixel scale
+    display.setTextColor(SSD1306_WHITE); // Draw white text
+    display.setCursor(0, 0);
+    display.println(String(frequency) + "hz");
+    display.setCursor(0,15);
+    display.println(String(dutycycle) + "%");
+    display.display();
+}
